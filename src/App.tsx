11efda143cbb75a10dfebbcf88c7472c33c3ba1e
Lary <larissa.mendy@gmail.com>
2026-04-13 10:51:01 -0300

    👌 Modificação do App

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer"
+import Home from "./page/home/Home"
+
+
+
+function App() {
+  return (
+   <>
+   <BrowserRouter>
+   <Navbar/>
+   <div className="min-h-[80vh]">
+    <Routes>
+      <Route path="/" element= {<Home/>} />
+    </Routes>
+   </div>
+    <Footer/>
+   </BrowserRouter>
+   
+   </>
+  )
+}
+
+export default App
